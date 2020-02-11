a338641b7e4f6fe24c37f9ffd29b59a1f6575569
Alberto Acosta <alberto.acosta@gohenry.co.uk>
2020-02-11 21:45:17 +0000

    adding item into array

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
-import React from 'react';
-import './App.css';
-import './imagebank/matrix.jpg'
-function App() {
+import React from "react";
+import "./App.css";
+import "./imagebank/matrix.jpg";
 
-    const proPerty = [1, 2, 3]
+export const App = () => {
+  // debugger;
+  // let proPerty = [];
+  // console.log("proPerty from rerender ", proPerty);
+  const [number, setNumber] = React.useState(0);
+  const [proPerty, setProPerty] = React.useState([]);
 
-  console.log(proPerty)
+  const salvarNumero = ({ value }) => {
+    // console.log(value);
+    setNumber(value);
+  };
+
+  const onAdd = () => {
+    console.log("from on add, number is eq to: ", number);
+    // proPerty.push("proPerty from event ", proPerty);
+    // console.log(number);
+
+    setProPerty([number, ...proPerty]);
+  };
+
+  console.log("from re-render number is eq to : ", number);
 
   return (
-    <div>
+    <div style={{ margin: 15 }}>
       <h1>Rendering arrays and matrices</h1>
       {/* <img>src={matrix.jpg}/img> alt={'Image not displayed'} */}
-
-      <form>
-        <label>Value</label><br/>
-
-        <input
-          type="text"
-          id="input" 
-          name= "input"
-          //onChange={newVal}
-          
-        />
-
-        <button
-          type="submit"
-          name="button"
-          //onClick={onAdd()}
-        >
-          Submit!
-        </button><br />
-
-      </form>
-      {proPerty}
+      {/* <form> */}
+      <label>Value</label>
+      <br />
+      <input
+        type="number"
+        value={number}
+        onChange={e => salvarNumero(e.target)}
+      />
+      <button onClick={onAdd}>Submit!</button>
+      <br />
+      {/* </form> */}
+      proPerty= {proPerty}
     </div>
-
-
   );
-}
+};
 
-export default App;
+// export default App;
